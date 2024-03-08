4284a133a527cf000cfdd1de69dc09258be16e02
Matheus Rizzati <matheustassinaririzzati@gmail.com>
2024-03-07 21:41:02 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def search():
+    df = pd.read_excel('./tipi.xlsx')
+    df = df.dropna(subset=['ALIQUOTA'])
+
+    ncm = request.args.get('ncm')
+    
+    if ncm:
+        result = df.loc[df['NCM'] == ncm].to_dict(orient='records')
+    else:
+        result = df.to_dict(orient='records')
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
